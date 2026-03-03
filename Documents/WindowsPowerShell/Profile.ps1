5dee8588ff372b31269fe6440aab5f2282dc7e36
David <d.eckold@techmodal.com>
2026-03-03 15:05:35 +0000

    remove unneeded lines from profile.ps1

--- Documents/WindowsPowerShell/Profile.ps1
+++ Documents/WindowsPowerShell/Profile.ps1
@@ -11,7 +11,3 @@ $internal = Join-Path $onedrive 'internal'
 
 function dotfiles { git --git-dir=$HOME/.dotfiles --work-tree=$HOME $args}
 
-function AWS_CLI_LOCAL {docker run --rm -it -e AWS_ACCESS_KEY_ID=user -e AWS_SECRET_ACCESS_KEY=password -e AWS_ENDPOINT_URL=http://localhost:9000 -v $(pwd):/aws amazon/aws-cli:latest $args}
-Set-Alias -Name aws_local AWS_CLI_LOCAL
-function AWS_CLI_TMBRSSONI {docker run --rm -it -e AWS_ACCESS_KEY_ID=user -e AWS_SECRET_ACCESS_KEY=password -e AWS_ENDPOINT_URL=http://tmbrssoni01:9000 -v $(pwd):/aws amazon/aws-cli:latest $args}
-Set-Alias -Name aws_tmbrssoni AWS_CLI_TMBRSSONI

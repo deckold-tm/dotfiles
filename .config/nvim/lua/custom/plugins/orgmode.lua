b4bef39d53c1f7047cbe7ca9d0b54756c6e0b362
David Eckold <d.eckold@techmodal.com>
2026-03-26 10:03:16 +0000

    update orgmode config

--- .config/nvim/lua/custom/plugins/orgmode.lua
+++ .config/nvim/lua/custom/plugins/orgmode.lua
@@ -4,11 +4,47 @@ return {
 		event = "VeryLazy",
 		ft = { "org" },
 		opts = {},
-		-- config = function()
-		-- 	require("orgmode").setup({
-		-- 		-- org_agenda_files = '$home/OneDrive - Techmodal/orgfiles/**/*',
-		-- 		-- org_default_notes_file = '$home/OneDrive - Techomdal/orgiles/refile.org',
-		-- 	})
-		-- end,
+		config = function()
+			require("orgmode").setup({
+				org_agenda_files = "~/Documents/Notes/**/*",
+				org_default_notes_file = "~/Documents/Notes/refile.org",
+				org_capture_templates = {
+					r = {
+						description = "Repo",
+						template = "* [[%x][%(return string.match('%x', '([^/]+)$'))]]%?",
+						target = "~/Documents/Notes/repos.org",
+					},
+					a = {
+						description = "Journal Articles",
+						template = "* ARTICLE %t [[%x][%^{description|link}]]",
+						target = "~/Documents/Notes/articles.org",
+					},
+					n = {
+						description = "Generic Note",
+						template = "* NOTE %t %^{Title}",
+						target = "~/Documents/Notes/notes.org",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"chipsenkbeil/org-roam.nvim",
+		tag = "0.2.0",
+		dependencies = {
+			{
+				"nvim-orgmode/orgmode",
+				tag = "0.7.0",
+			},
+		},
+		config = function()
+			require("org-roam").setup({
+				directory = "~/Documents/org_roam/",
+				-- optional
+				org_files = {
+					"~/Documents/Notes/",
+				},
+			})
+		end,
 	},
 }
